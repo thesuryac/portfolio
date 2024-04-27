cd54eae3f71f687424cc0f88550ddd7234db0158
thesuryac <thesuryac@gmail.com>
2024-04-27 14:56:18 +0530

    added the scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,26 @@ import Projects from "./pages/Projects";
 import About from "./pages/About";
 import CallToAction from "./pages/CallToAction";
 import Header from "./components/Header";
+import "./index.css";
 
 const App = () => {
   return (
-    <div className="">
-      <main className="h-screen sm:w-[97vw] max-w-[1500px] mx-auto snap-y snap-mandatory">
-        <Header />
+    <main className="h-screen sm:w-[97vw] max-w-[1500px] mx-auto  snap-y snap-mandatory  overflow-y-auto no-scrollbar ease-in-out delay-200">
+      <Header />
 
-        <div className="snap-start h-screen">
-          <Hero />
-        </div>
-        <div className="snap-start h-screen">
-          <Projects />
-        </div>
-        <div className="snap-start h-screen">
-          <About />
-        </div>
-        <div className="snap-start h-screen">
-          <CallToAction />
-        </div>
-      </main>
-    </div>
+      <div className="snap-center h-screen">
+        <Hero />
+      </div>
+      <div className="snap-center h-screen ">
+        <Projects />
+      </div>
+      <div className="snap-center h-screen ">
+        <About />
+      </div>
+      <div className="snap-center h-screen ">
+        <CallToAction />
+      </div>
+    </main>
   );
 };
 
